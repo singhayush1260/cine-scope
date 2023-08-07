07e85cd73f2d021911cdbf4ae69e4ea1e32aa43a
Ayush Singh <singh.dev6537@gmail.com>
2023-08-07 22:16:06 +0530

    added details page

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,6 @@
 import "./style.scss";
 import { useRef } from "react";
-import {
-  BsFillArrowLeftCircleFill,
-  BsFillArrowRightCircleFill,
-} from "react-icons/bs";
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill,} from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
@@ -12,37 +9,20 @@ import Img from "../lazy-load-image/Img";
 import PosterFallback from "../../assets/images/no-poster.png";
 import CircleRating from "../circle-rating/CircleRating";
 import Genres from "../genres/Genres";
+
 const Carousel = ({ data, loading, endpoint }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {
-    const container = carouselContainer.current;
-    console.log(dir, container)
-
-    const scrollAmount =
-      dir === "left"
-        ? container.scrollLeft - (container.clientWidth + 20)
-        : container.scrollLeft + (container.clientWidth + 20);
-
-        console.log(scrollAmount)
-
-    container.scrollTo({ left: scrollAmount, behavior: "smooth" });
-  };
-
   const showPrev = () => {
     let width = carouselContainer.current.clientWidth;
-    console.log(width);
     carouselContainer.current.scrollLeft -= width;
-    console.log('prev');
   };
 
   const showNext = () => {
     let width = carouselContainer.current.clientWidth;
-    console.log(width);
     carouselContainer.current.scrollLeft += width;
-    console.log('next');
   };
 
   const skItem = () => {
@@ -59,22 +39,15 @@ const Carousel = ({ data, loading, endpoint }) => {
   return (
     <div className="carousel">
       <ContentWrapper>
-        <BsFillArrowLeftCircleFill
-          className="carouselLeftNav arrow"
-          onClick={() => showPrev()}
-        />
-        <BsFillArrowRightCircleFill
-          className="carouselRighttNav arrow"
-          onClick={() => showNext()}
-        />
+        <BsFillArrowLeftCircleFill className="carouselLeftNav arrow" onClick={() => showPrev()}/>
+        <BsFillArrowRightCircleFill className="carouselRighttNav arrow" onClick={() => showNext()} />
         {!loading ? (
           <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
-              const posterUrl = item.poster_path
-                ? url.poster + item.poster_path
-                : PosterFallback;
+              const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
               return (
-                <div key={item.id} className="carouselItem">
+                <div key={item.id} className="carouselItem"
+                onClick={() =>navigate(`/${item.media_type || endpoint}/${ item.id}`)}>
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
@@ -83,9 +56,7 @@ const Carousel = ({ data, loading, endpoint }) => {
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
                     <span className="date">
-                      {dayjs(item.release_date || item.first_air_date).format(
-                        "MMM D, YYYY"
-                      )}
+                      {dayjs(item.release_date || item.first_air_date).format( "MMM D, YYYY" )}
                     </span>
                   </div>
                 </div>
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,21 @@
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 import './style.scss'
+import DetailsBanner from './details-banner/DetailsBanner';
+import Cast from './cast/Cast';
 const Details=()=>{
+
+const {mediaType, id}= useParams();    
+
+const {data, loading}=useFetch(`/${mediaType}/${id}/videos`)
+
+const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
     return(
         <>
-        Details</>
+        <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+        <Cast data={credits?.cast} loading={creditsLoading} />
+        </>
     )
 }
 export default Details;
